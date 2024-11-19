83f8ba24fd72956265ee0c52a73304fc04d9dada
gmh4143123 <kl7519@naver.com>
2024-11-19 15:58:37 +0900

    경민 gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,13 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.9.0")
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation ("androidx.core:core-ktx:1.10.1")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:19.0.0")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 }
