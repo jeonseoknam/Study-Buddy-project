43f38d6e4e00cb8b52bb931c0bbe2dcbc27abab6
gmh4143123 <kl7519@naver.com>
2024-11-20 14:35:11 +0900

    앱 재실행시 로그인 유지 / 로그인화면에서 회원가입화면으로 전환

--- app/src/main/java/com/example/studybuddy/LoginActivity.java
+++ app/src/main/java/com/example/studybuddy/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.studybuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,8 +32,32 @@ public class LoginActivity extends AppCompatActivity {
 
         Button loginButton = findViewById(R.id.btn_Login);
         loginButton.setOnClickListener(v -> performLogin());
+        //회원가입 화면으로 이동
+        TextView gotoSignup = findViewById(R.id.toSignUpText);
+        gotoSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    //로그인 유지용
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null){
+            updateUI(user);
+        }
+    }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
     private void performLogin() {
         String email = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
         String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
@@ -39,16 +66,13 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "이메일과 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
             return;
         }
-
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show();
-
-                        // 메인 화면으로 이동
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
+                        //메인으로 이동
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
                     } else {
                         Toast.makeText(this, "로그인 실패: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
