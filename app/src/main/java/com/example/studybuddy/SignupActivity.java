587624f54f1c0f39008aa7c921cfd4faa4a2144a
gmh4143123 <kl7519@naver.com>
2024-11-20 14:37:12 +0900

    모든 액티비에서 접근 가능한 userData 자바파일 생성(이름,메일 등) //회원가입 시 받는 정보를 종합해서 firestore에 저장

--- app/src/main/java/com/example/studybuddy/SignupActivity.java
+++ app/src/main/java/com/example/studybuddy/SignupActivity.java
@@ -2,6 +2,8 @@ package com.example.studybuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,46 +15,74 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignupActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
+    private String name, school, nickname, email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
         mAuth = FirebaseAuth.getInstance(); // FirebaseAuth 초기화
+        db = FirebaseFirestore.getInstance();
+        name = null; school = null; nickname = null; email = null; password = null;
 
         Button signUpButton = findViewById(R.id.btn_signUp);
-        signUpButton.setOnClickListener(v -> performSignUp());
-
-
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performSignUp();
+            }
+        });
     }
 
     private void performSignUp() {
-        String email = ((EditText) findViewById(R.id.editTextEmail2)).getText().toString();
-        String password = ((EditText) findViewById(R.id.editTextPassword2)).getText().toString();
+        name = ((EditText)findViewById(R.id.editTextUsername)).getText().toString();
+        school = ((EditText)findViewById(R.id.editTextSchoolName)).getText().toString();
+        nickname = ((EditText)findViewById(R.id.editTextNickName)).getText().toString();
+        email = ((EditText)findViewById(R.id.editTextEmail2)).getText().toString();
+        password = ((EditText) findViewById(R.id.editTextPassword2)).getText().toString();
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "이메일과 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
-            return;
+        HashMap<String , Object> userdata = new HashMap<>();
+        userdata.put("Name", name);
+        userdata.put("School", school);
+        userdata.put("Nickname", nickname);
+        userdata.put("Email", email);
+        userdata.put("Password", password);
+
+        if ((name != null && !name.isEmpty()) &&
+                (school != null && !school.isEmpty()) &&
+                (nickname != null && !nickname.isEmpty()) &&
+                (email != null && !email.isEmpty()) &&
+                (password != null && !password.isEmpty()) )
+        {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            userdata.put("UID", user.getUid());
+                            userdata.put("ProfileImage", "none");
+                            db.collection("userInfo").document(user.getUid()).set(userdata);
+                            Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                            // 추가적인 정보 저장을 원한다면 Firebase Realtime Database 또는 Firestore를 사용
+                        } else {
+                            Toast.makeText(this, "회원가입 실패: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "다시 입력해주세요.", Toast.LENGTH_SHORT).show();
         }
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
-
-                        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                        // 추가적인 정보 저장을 원한다면 Firebase Realtime Database 또는 Firestore를 사용
-                    } else {
-                        Toast.makeText(this, "회원가입 실패: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
+
 }
---
+++ app/src/main/java/com/example/studybuddy/utility/userData.java
@@ -0,0 +1,9 @@
+package com.example.studybuddy.utility;
+
+public class userData {
+    public static String userName;
+    public static String userSchool;
+    public static String userNickname;
+    public static String profileURI;
+
+}
